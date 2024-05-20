c00582f8fb56718e7a6e9a5f3bc9178abfc242ba
ShonenMind <122054630+ShonenMind@users.noreply.github.com>
2024-05-20 14:36:44 -0700

    huge changes

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new Typewriter(".typing", {
+    strings: [
+      "a software engineer",
+      "sleeping",
+      "climbing walls",
+      "yapping",
+      "making projects I'll never finish",
+      "playing guitar",
+    ],
+    autoStart: true,
+    loop: true,
+  });
